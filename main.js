b6c83dddb0aab865acb5a3477fba0b428d990c30
Andrew Jia <andrew.jia.2013@gmail.com>
2022-12-16 01:02:38 -0800

    Update main.js
    finished summoner input, wr% calculator

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const api_key = 'RGAPI-0982a1ae-4980-4d33-84b3-4ed945bbf8fd';
 const testSummonerName = 'moustachetacohat';
 const testPUUID = 'Rvs-ajlKaiugM3CT4Rw39Jm6THVZqgJjRZAOeI-L0PgxqzguB_LOsXZC0H9oJeuE5NWYJ5auIu7Xsg';
 
-const startTime = 1669136400;
+const startTime = 1669136400; //approx release time of patch 12.22 in NA
 
 const jakshoID = 6665;
 
@@ -11,14 +11,14 @@ const jakshoID = 6665;
 //getMatchBatch(testPUUID, 0, 'ranked');
 //analyzeMatch('NA1_4499608106');          //game with no jaksho
 //analyzeMatch('NA1_4513324909');          //game with one jaksho on losing team
-
+//getAllMatches(testPUUID, 'ranked');
 
 // input summoner name
 // output PUUID from riot
 async function getPUUID(summonerName) {
 	let response = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${api_key}`);
 	let resp_json = await response.json();
-	debugLog(`PUUID of ${summonerName} is ${resp_json['puuid']}`);
+	//debugLog(`PUUID of ${summonerName} is ${resp_json['puuid']}`);
 	
 	return resp_json['puuid'];
 }
@@ -30,7 +30,22 @@ async function getMatchBatch(PUUID, startInd, type) {
 	let response = await fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${PUUID}/ids?startTime=${startTime}&type=${type}&start=${startInd}&count=100&api_key=${api_key}`);
 	let matchBatch = await response.json();
 	//debugLog(matchBatch);
+
 	return matchBatch;
+}
+
+async function getAllMatches(PUUID, type) {
+	let allMatches = [];
+	let currInd = 0;
+	let currBatch = await getMatchBatch(PUUID, currInd, type);
+	while(currBatch.length == 100) {
+		allMatches = allMatches.concat(currBatch);
+		currBatch = await getMatchBatch(PUUID, currInd, type);
+		currInd += 100;
+	}
+	allMatches = allMatches.concat(currBatch);
+	
+	return allMatches;
 }
 
 // input match ID
@@ -52,14 +67,31 @@ async function analyzeMatch(matchId) {
 			}
 		}
 	}
-	debugLog(`${matchId} ${output}`);
+	//debugLog(`${matchId} ${output}`);
 	return output;
 }
 
 // submit button event listener
 const submitButton = document.getElementById('submitButton');
-submitButton.addEventListener("click", function() {
-	debugLog(`input summoner name is ${document.getElementById('SummName').value}`)
+submitButton.addEventListener("click", async function() {
+	let summonerName = document.getElementById('SummName').value;
+	debugLog(`input summoner name is ${summonerName}`);
+	let PUUID = await getPUUID(summonerName);
+	debugLog(`PUUID = ${PUUID}`);
+	let allMatches = await getAllMatches(PUUID, 'ranked');
+	debugLog(`found ${allMatches.length} matches`);
+
+	let winLoss = [0, 0];
+	for(const match of allMatches) {
+		let matchResult = await analyzeMatch(match);
+		//debugLog(matchResult);
+		winLoss[0] += matchResult[0];
+		winLoss[1] += matchResult[1];
+	}
+
+	debugLog(`the total win/loss of Jak'Sho is ${winLoss[0]}/${winLoss[1]}`);
+	let winrate = (100 * winLoss[0]) / (winLoss[0] + winLoss[1]);
+	debugLog(`winrate is ${winrate}`);
 });
 
 // easy way to toggle all console log messages
