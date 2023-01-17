984c10f82c6a377a7d209c37ce44344f55466f58
Andrew Jia <andrew.jia.2013@gmail.com>
2023-01-16 20:16:40 -0800

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const testSummonerName = 'moustachetacohat';
 const testPUUID = 'Rvs-ajlKaiugM3CT4Rw39Jm6THVZqgJjRZAOeI-L0PgxqzguB_LOsXZC0H9oJeuE5NWYJ5auIu7Xsg';
 
 const startTime = 1669136400; //approx release time of patch 12.22 in NA
-
 const jakshoID = 6665;
+const unspokenID = 7026;
 
 //getPUUID(testSummonerName);
 //getMatchBatch(testPUUID, 0, 'ranked');
@@ -27,7 +27,8 @@ async function getPUUID(summonerName) {
 // input PUUID, query start index, queue type
 // output list of match IDs, 0 <= length <= 100
 async function getMatchBatch(PUUID, startInd, type) {
-	let response = await fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${PUUID}/ids?startTime=${startTime}&type=${type}&start=${startInd}&count=100&api_key=${api_key}`);
+	const batchSize = 100;
+	let response = await fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${PUUID}/ids?startTime=${startTime}&type=${type}&start=${startInd}&count=${batchSize}&api_key=${api_key}`);
 	let matchBatch = await response.json();
 	//debugLog(matchBatch);
 
@@ -38,11 +39,14 @@ async function getAllMatches(PUUID, type) {
 	let allMatches = [];
 	let currInd = 0;
 	let currBatch = await getMatchBatch(PUUID, currInd, type);
+	
 	while(currBatch.length == 100) {
+		//debugLog(`batch size of ${currBatch.length}`);
 		allMatches = allMatches.concat(currBatch);
 		currBatch = await getMatchBatch(PUUID, currInd, type);
 		currInd += 100;
 	}
+	//debugLog(`batch size of ${currBatch.length}`);
 	allMatches = allMatches.concat(currBatch);
 	
 	return allMatches;
@@ -50,19 +54,28 @@ async function getAllMatches(PUUID, type) {
 
 // input match ID
 // output array
-// [win team JS count, loss team JS count]
-async function analyzeMatch(matchId) {
+// [win team JS count, loss team JS count, personal win w/ JS, personal loss w/ JS]
+async function analyzeMatch(matchId, summonerName) {
 	let response = await fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${api_key}`);
 	let matchInfo = await response.json();
 
-	let output = [0, 0];
+	//debugLog(matchInfo['info']['participants'][0]['summonerName']);
+	//debugLog(matchInfo['info']['participants'][0])
+
+	let output = [0, 0, 0, 0];
 	for(let i = 0; i < 10; i++) {
 		for(let j = 0; j < 6; j++) {
-			if(matchInfo['info']['participants'][i][`item${j}`] == jakshoID) {
+			if([jakshoID, unspokenID].includes(matchInfo['info']['participants'][i][`item${j}`])) {
 				if(matchInfo['info']['participants'][i]['win']) {
 					output[0]++;
+					if(matchInfo['info']['participants'][0]['summonerName'] == summonerName) {
+						output[2]++;
+					}
 				} else {
 					output[1]++;
+					if(matchInfo['info']['participants'][0]['summonerName'] == summonerName) {
+						output[3]++;
+					}
 				}
 			}
 		}
@@ -74,25 +87,49 @@ async function analyzeMatch(matchId) {
 // submit button event listener
 const submitButton = document.getElementById('submitButton');
 submitButton.addEventListener("click", async function() {
-	let summonerName = document.getElementById('SummName').value;
+	const queueType = getRadio();
+	const summonerName = document.getElementById('SummName').value;
 	debugLog(`input summoner name is ${summonerName}`);
-	let PUUID = await getPUUID(summonerName);
+	const PUUID = await getPUUID(summonerName);
 	debugLog(`PUUID = ${PUUID}`);
-	let allMatches = await getAllMatches(PUUID, 'ranked');
-	debugLog(`found ${allMatches.length} matches`);
+	const allMatches = await getAllMatches(PUUID, queueType);
+	debugLog(`found ${allMatches.length} matches of type ${queueType}`);
 
 	let winLoss = [0, 0];
+	let personalWL = [0, 0]
 	for(const match of allMatches) {
-		let matchResult = await analyzeMatch(match);
-		//debugLog(matchResult);
+		await delay(800);             //limit to < 1 req / second
+		let matchResult = await analyzeMatch(match, summonerName);
 		winLoss[0] += matchResult[0];
 		winLoss[1] += matchResult[1];
+		personalWL[0] += matchResult[2];
+		personalWL[1] += matchResult[3];
+		debugLog('analyzed match');
 	}
 
 	debugLog(`the total win/loss of Jak'Sho is ${winLoss[0]}/${winLoss[1]}`);
 	let winrate = (100 * winLoss[0]) / (winLoss[0] + winLoss[1]);
 	debugLog(`winrate is ${winrate}`);
+	debugLog(`your personal win/loss with Jak'Sho is ${personalWL[0]}/${personalWL[1]}`)
 });
+
+// get radio button value
+function getRadio() {
+	let radios = document.getElementsByName('QueueType');
+	for(let i = 0; i < radios.length; i++) {
+		if(radios[i].checked) {
+			debugLog(`selected queue type is ${radios[i].value}`);
+			return radios[i].value;
+		}
+	}
+
+	// none selected, default to all queue types
+	return "";
+}
+
+function delay(time) {
+	return new Promise(resolve => setTimeout(resolve, time));
+}
 
 // easy way to toggle all console log messages
 function debugLog(msg) {
